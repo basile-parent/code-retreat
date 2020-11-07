612b1d887014ad248e733a08c9c0fd3e75ea4b1b
Casou <casou.acc@gmail.com>
2020-11-07 16:13:53 +0100

    Step

--- src/main/java/com/example/coderetreat/GameOfLife.java
+++ src/main/java/com/example/coderetreat/GameOfLife.java
@@ -1,5 +1,8 @@
 package com.example.coderetreat;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class GameOfLife {
 
     private Cellule[][] cells;
@@ -13,8 +16,63 @@ public class GameOfLife {
         }
     }
 
+    public void step() {
+        for(int i = 0; i < cells.length; i++) {
+            for(int j = 0; j < cells[i].length; j++) {
+                Cellule currentCell = this.cells[i][j];
+                boolean newIsAliveStatus = currentCell.isAlive();
+
+                Set<Cellule> adjacentCells = getAdjacentCells(i, j);
+                long nbAlives = adjacentCells.stream().filter(Cellule::isAlive).count();
+
+                if (currentCell.isAlive()) {
+                    if (nbAlives < 2) {
+                        newIsAliveStatus = false;
+                    }
+                }
+
+                currentCell.setAlive(newIsAliveStatus);
+            }
+        }
+    }
+
+    private Set<Cellule> getAdjacentCells(int i, int j) {
+        Set<Cellule> adjacents = new HashSet<>();
+
+        if (i > 0) {
+            adjacents.add(this.cells[i-1][j]);
+            if (j > 0) {
+                adjacents.add(this.cells[i-1][j-1]);
+            }
+            if (j < this.cells[i].length - 1) {
+                adjacents.add(this.cells[i-1][j+1]);
+            }
+        }
+        if (i < this.cells.length - 1) {
+            adjacents.add(this.cells[i+1][j]);
+            if (j > 0) {
+                adjacents.add(this.cells[i+1][j-1]);
+            }
+            if (j < this.cells[i].length - 1) {
+                adjacents.add(this.cells[i+1][j+1]);
+            }
+        }
+        if (j > 0) {
+            adjacents.add(this.cells[i][j-1]);
+        }
+        if (j < this.cells[i].length - 1) {
+            adjacents.add(this.cells[i][j+1]);
+        }
+
+        return adjacents;
+    }
+
     public void setCell(int row, int col, Cellule cell) {
         this.cells[row][col] = cell;
+    }
+
+    public Cellule[][] getCells() {
+        return this.cells;
     }
 
     public String toString() {
--- src/test/java/com/example/coderetreat/GameOfLifeTest.java
+++ src/test/java/com/example/coderetreat/GameOfLifeTest.java
@@ -33,4 +33,34 @@ class GameOfLifeTest {
         assertEquals("[X]\n", result);
     }
 
+    @Test
+    public void toString_should_return_a_string_representation_of_game_of_life_when_have_several_lines() {
+        // Given
+        GameOfLife gol = new GameOfLife(2, 2);
+        gol.setCell(0, 0, new Cellule(true));
+        gol.setCell(1, 1, new Cellule(true));
+
+        // When
+        String result = gol.toString();
+
+        // Then
+        assertEquals("[X][ ]\n[ ][X]\n", result);
+    }
+
+    @Test
+    public void step_should_kill_a_cell_if_it_has_less_than_two_alive_adjacent_cells() {
+        // Given
+        // [X][ ]
+        // [ ][ ]
+        GameOfLife gol = new GameOfLife(2, 2);
+        gol.setCell(0, 0, new Cellule(true));
+
+        // When
+        gol.step();
+
+        // Then
+        Cellule[][] cells = gol.getCells();
+        assertFalse(cells[0][0].isAlive());
+    }
+
 }
