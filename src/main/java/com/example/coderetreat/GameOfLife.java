777291353ad9a717cf4f53a5f0b86d719c59aee5
Casou <casou.acc@gmail.com>
2020-11-07 15:19:27 +0100

    First test

--- src/main/java/com/example/coderetreat/GameOfLife.java
+++ src/main/java/com/example/coderetreat/GameOfLife.java
@@ -2,8 +2,27 @@ package com.example.coderetreat;
 
 public class GameOfLife {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private Cellule[][] cells;
+
+    public GameOfLife(int rows, int cols) {
+        this.cells = new Cellule[rows][cols];
+        for(int i = 0; i < cells.length; i++) {
+            for(int j = 0; j < cells[i].length; j++) {
+                this.cells[i][j] = new Cellule();
+            }
+        }
+    }
+
+    public String toString() {
+        StringBuilder str = new StringBuilder("");
+        for(int i = 0; i < cells.length; i++) {
+            for(int j = 0; j < cells[i].length; j++) {
+                str.append(String.format("[%s]", cells[i][j]));
+            }
+            str.append("\n");
+        }
+
+        return str.toString();
     }
 
 }
