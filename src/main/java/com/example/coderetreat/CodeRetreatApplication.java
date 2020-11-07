5a6480fa7b58c35a6874760bf82223dd80ef11e5
Casou <casou.acc@gmail.com>
2020-11-07 15:02:30 +0100

    First commit

---
+++ src/main/java/com/example/coderetreat/CodeRetreatApplication.java
@@ -0,0 +1,13 @@
+package com.example.coderetreat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeRetreatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeRetreatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coderetreat/CodeRetreatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coderetreat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeRetreatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
